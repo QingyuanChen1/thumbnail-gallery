42e45ac00c0ac7a651a88356a67f7a62be372cdf
Qingyuan Chen <Qingyuan.Chen2@georgebrown.ca>
2021-12-04 07:05:23 -0500

    final edition

--- js/script.js
+++ js/script.js
@@ -2,15 +2,10 @@ const lg = document.querySelector(`#lrgimg`);
 const t1 = document.querySelector(`#th1`);
 const t2 = document.querySelector(`#th2`);
 const t3 = document.querySelector(`#th3`);
-
+const thumbs = document.querySelectorAll(`.thumbnail`)
 const clickfunc = function(event){
-    const thumb = event.target;
-    const url = thumb.getAttribute(`src`);
-    lg.setAttribute(`src`,url);
-    changeofimage(event.target)
+    lg.setAttribute(`src`,event.target.getAttribute(`src`));
 };
-t1.addEventListener(`click`,clickfunc);
-t2.addEventListener(`click`,clickfunc);
-
-t3.addEventListener(`click`,clickfunc);
-
+thumbs.forEach(function(thumbs){
+    thumbs.addEventListener(`click`,clickfunc);
+})
