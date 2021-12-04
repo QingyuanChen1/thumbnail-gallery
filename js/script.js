c70357ee4755b6c8c3cf80d661f91b8d3185a5fb
Qingyuan Chen <Qingyuan.Chen2@georgebrown.ca>
2021-12-04 06:47:17 -0500

    The most Efficent way of writing it

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const t1 = document.querySelector(`#th1`);
 const t2 = document.querySelector(`#th2`);
 const t3 = document.querySelector(`#th3`);
 
+const changeofimage = function(thumb){
+    const url = thumb.getAttribute(`src`);
+    lg.setAttribute(`src`,url)
+};
+const clickfunc = function(event){
+    changeofimage(event.target)
+};
+t1.addEventListener(`click`,clickfunc);
+t2.addEventListener(`click`,clickfunc);
 
-t1.addEventListener(`click`,function(){
-    lg.setAttribute(`src`,`img/one.jpg`);
-})
-t2.addEventListener(`click`,function(){
-    lg.setAttribute(`src`,`img/two.jpg`);
-})
-
-t3.addEventListener(`click`,function(){
-    lg.setAttribute(`src`,`img/three.jpg`);
-})
+t3.addEventListener(`click`,clickfunc);
 
